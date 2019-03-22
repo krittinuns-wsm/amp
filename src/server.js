a5e713dce01aa561caf3ea18237305a6e883be42
Krittinun Sirodom <Krittinun.Sirodom@rockyou.com>
2019-03-22 11:34:39 +0700

    fix eslint warnings

--- src/server.js
+++ src/server.js
@@ -1,34 +1,33 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import Post from "./pages/post";
-import _ from 'lodash'
+import Post from './pages/post'
 import { ORIGINAL_SITE } from './configs/site'
 
 dotenv.config()
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 app.get('/*/amp', async (req, res) => {
   try {
-    const urlArray = req.path.split("/")
-    const slug = urlArray[urlArray.length-2]
+    const urlArray = req.path.split('/')
+    const slug = urlArray[urlArray.length - 2]
 
     const data = await Post(slug)
     res.render('post', data)
   } catch (error) {
     res.redirect(ORIGINAL_SITE)
-  }  
+  }
 })
 
 // 404
-app.use(function(req, res, next) {
-  return res.status(404).send({ message: 'Route'+req.url+' Not found.' })
-});
+app.use(function (req, res, next) {
+  return res.status(404).send({ message: 'Route' + req.url + ' Not found.' })
+})
 
 // 500 - Any server error
-app.use(function(err, req, res, next) {
-  return res.status(500).send({ error: err });
-});
+app.use(function (err, req, res, next) {
+  return res.status(500).send({ error: err })
+})
 
 app.listen(3000, () => console.log('listening on port 3000'))
